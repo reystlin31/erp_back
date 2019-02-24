3c1feaef707a535c14bcf9ed20cfa24666fbdc5c
Reystlin <reystlin31@yandex.ru>
2019-02-24 11:42:01 +0300

    first

--- Functions.php
+++ Functions.php
@@ -9,56 +9,6 @@ function generateCode($length=6)
         $code .= $chars[mt_rand(0,$clen)];
     return $code;
 }
-//Шаблонизатор
-function Load_Page($url)
-{
-    $options = array(
-        CURLOPT_RETURNTRANSFER => true,     // return web page
-        CURLOPT_HEADER         => false,    // don't return headers
-        CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-        CURLOPT_ENCODING       => "",       // handle all encodings
-        CURLOPT_USERAGENT      => "spider", // who am i
-        CURLOPT_AUTOREFERER    => true,     // set referer on redirect
-        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-        CURLOPT_TIMEOUT        => 120,      // timeout on response
-        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
-        CURLOPT_SSL_VERIFYPEER => false     // Disabled SSL Cert checks
-    );
-
-    $ch      = curl_init( $url );
-    curl_setopt_array( $ch, $options );
-    $content = curl_exec( $ch );
-    $err     = curl_errno( $ch );
-    $errmsg  = curl_error( $ch );
-    $header  = curl_getinfo( $ch );
-    curl_close( $ch );
-
-    $header['errno']   = $err;
-    $header['errmsg']  = $errmsg;
-    $header['content'] = $content;
-    if($err)
-    {
-        echo "Error ".$err.":".$errmsg;
-        exit();
-    }
-    return $content;
-}
-
-function Template($tmp,$vars = array())
-{
-    if(file_exists('Templates/'.$tmp))
-    {
-        ob_start();
-        extract($vars);
-        require 'Templates/'.$tmp;
-        return ob_get_clean();
-    }
-    else
-    {
-        echo "Шаблон ".$tmp." не найден";
-        exit();
-    }
-}
 
 function _requestStatus($code)
 {
